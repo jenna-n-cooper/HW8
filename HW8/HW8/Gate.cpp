a14ec9adc3db3f2f4c723fc590c18189e37f4429
jenna-n-cooper <jennacooper207@gmail.com>
2024-11-11 16:30:42 -0500

    did some gate stuff

--- HW8/HW8/Gate.cpp
+++ HW8/HW8/Gate.cpp
@@ -4,24 +4,47 @@
 
 Gate::Gate(Wire* input1, Wire* input2, Wire* output, int d, string t)
 {
+	in1 = input1;//do I need to do a deep copy here?
+	in2 = input2;
+	out = output;
+	delay = d;
+	type = t;
+
+
 }
 
 int Gate::getDelay() const
 {
-	return 0;
+	return delay;
 }
 
 Wire* Gate::getInput(int num) const
 {
-	return nullptr;
+	if (num == 1) {
+		return in1;
+	}
+	else if (num == 2) {
+		return in2;
+	}
+	else {
+		return nullptr;
+	}
+	
 }
 
 Wire* Gate::getOutput() const
 {
-	return nullptr;
+	return out;
 }
 
 char Gate::returnVal(Wire* input1, Wire* input2, Wire* output, int d, string t) const
 {
+	if (t == "AND") {
+
+	}
+	else if (t == "OR") {
+
+	}
+
 	return 0;
 }
