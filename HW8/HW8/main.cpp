55c186b2efd7933bea3d7d9ac6dd00b9cb35045b
abigailhaut <ajhaut0528@gmail.com>
2024-11-12 18:32:46 -0500

    changes

--- HW8/HW8/main.cpp
+++ HW8/HW8/main.cpp
@@ -3,9 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include "Wire.h"
-//#include "Wire.cpp"
 #include "Gate.h"
-//#include "Gate.cpp"
 
 using namespace std;
 
@@ -40,8 +38,8 @@ int main() {
 	Wire* newWire;
 
 	string line;
-	string ioo;
 	
+	string ioo;
 	string wn;
 	int wnum;
 	
