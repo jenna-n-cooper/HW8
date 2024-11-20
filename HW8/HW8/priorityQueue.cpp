4913447243ce5e10a1bec171e1f9806cf168b112
abigailhaut <ajhaut0528@gmail.com>
2024-11-19 20:32:36 -0500

    priorityQueue

---
+++ HW8/HW8/priorityQueue.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "priorityQueue.h"
+
+using namespace std;
+
+
+bool operator<(const priorityQueue& p1, const priorityQueue& p2)
+{
+	// by default the queue is a MAX queue, so we reverse the comparison
+// from '<' to '>' (see the return statements) to make it a MIN queue
+	if (p1.key == p2.key) {
+		return p1.secondaryKey > p2.secondaryKey;
+	}
+
+	return p1.key > p2.key;
+}
+
--- HW8/HW8/priorityQueue.h
+++ HW8/HW8/priorityQueue.h
@@ -26,7 +26,7 @@ class priorityQueue {
 		void   setEvent(Event* events) { pqEvents = events; }
 		
 		//string getString()    const { return s; }
-		friend bool operator<(const priorityQueue& p1, const priorityQueue& p2);
+		friend bool operator <(const priorityQueue& p1, const priorityQueue& p2);
 	
 	private:
 		int   key;
@@ -37,14 +37,3 @@ class priorityQueue {
 		// make for event class
 		
 };
-
-bool operator<(const priorityQueue& p1, const priorityQueue& p2) {
-
-	// by default the queue is a MAX queue, so we reverse the comparison
-	// from '<' to '>' (see the return statements) to make it a MIN queue
-	if (p1.key == p2.key) {
-		return p1.secondaryKey > p2.secondaryKey;
-	}
-
-	return p1.key > p2.key;
-}
